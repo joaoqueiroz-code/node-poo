268a6554fb126b9bed1a651843467dccfd737735
João Queiroz <joaoqueiroz-code@gmail.com>
2021-06-04 17:52:11 -0300

    atribuição de uma função a uma classe

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
-// Classe com o formato dos dados.
+// Classe dos dados do cliente.
 class Cliente{
     nome;
     cpf;
-    agencia;
-    saldo;
 
 }
 
-//Criação das contantes dos clientes. 
+// Classe dos dados do banco do cliente.
+class ContaCorrente {
+    agencia;
+    saldo;
+
+    sacar(valor) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    }
+}
+
+
+// Criação das constantes dos clientes. 
 const cliente1 = new Cliente();
 const cliente2 = new Cliente();
 
-// Dados do clientes.
+const contaCorrentecliente1 = new ContaCorrente();
+const contaCorrentecliente2 = new ContaCorrente();
+
+// Dados do cliente número um.
 cliente1.nome = "Yorick";
 cliente1.cpf = 65675778987;
-cliente1.agencia = 3004;
-cliente1.saldo = 0;
 
+contaCorrentecliente1.agencia = 3004;
+contaCorrentecliente1.saldo = 200;
+
+// Dados do cliente número dois.
 cliente2.nome = "Hecarim";
 cliente2.cpf = 77733311144;
-cliente2.agencia = 3004;
-cliente2.saldo = 0;
+
+contaCorrentecliente2.agencia = 3004;
+contaCorrentecliente2.saldo = 0;
 
 
+contaCorrentecliente1.sacar(50);
 
 
-
-console.log(cliente1, cliente2);
+console.log(contaCorrentecliente1.saldo);
