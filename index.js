a1e4994d8c33c61ab883d61fc68f65fcb00ca457
João Queiroz <joaoqueiroz-code@gmail.com>
2021-06-10 16:58:18 -0300

    new way to use a class

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-// Classe dos dados do cliente.
-class Cliente{
-    nome;
-    cpf;
-
-}
-
-// Classe dos dados do banco do cliente.
-class ContaCorrente {
-    agencia;
-    saldo;
-
-    sacar(valor) {
-        if(this.saldo >= valor) {
-            this.saldo -= valor;
-        }
-    }
-}
-
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
 // Criação das constantes dos clientes. 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
+//const cliente2 = new Cliente();
 
 const contaCorrentecliente1 = new ContaCorrente();
 const contaCorrentecliente2 = new ContaCorrente();
@@ -30,17 +13,24 @@ cliente1.nome = "Yorick";
 cliente1.cpf = 65675778987;
 
 contaCorrentecliente1.agencia = 3004;
-contaCorrentecliente1.saldo = 200;
+contaCorrentecliente1._saldo = 200;
+contaCorrentecliente1.cliente = cliente1;
 
 // Dados do cliente número dois.
-cliente2.nome = "Hecarim";
+
+/*cliente2.nome = "Hecarim";
 cliente2.cpf = 77733311144;
 
 contaCorrentecliente2.agencia = 3004;
-contaCorrentecliente2.saldo = 0;
+contaCorrentecliente2.cliente = cliente2;
+contaCorrentecliente2._saldo = 0;*/
+
+// Mesmo resultado que anteriormente porém feito de forma mais direta.
+contaCorrentecliente2.cliente = new Cliente();
+contaCorrentecliente2.cliente.nome = "Hecarim";
+contaCorrentecliente2.cliente.cpf = 77733311144;
+contaCorrentecliente2.agencia = 3001;
+contaCorrentecliente2._saldo = 200;
 
 
-contaCorrentecliente1.sacar(50);
-
-
-console.log(contaCorrentecliente1.saldo);
+console.log(contaCorrentecliente2);
