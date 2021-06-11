fc53fc13643c45ef5869f56076907ec0d921f694
João Queiroz <joaoqueiroz-code@gmail.com>
2021-06-11 09:04:35 -0300

    update in class methods

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-// Criação das constantes dos clientes. 
-const cliente1 = new Cliente();
-//const cliente2 = new Cliente();
+//TERCEIRA FORMA, UTILIZANDO CONSTRUCTOR.
+const cliente1 = new Cliente("Yorick", 65675778987);
+const cliente2 = new Cliente("Hecarim", 77733311144);
 
-const contaCorrentecliente1 = new ContaCorrente();
-const contaCorrentecliente2 = new ContaCorrente();
+const contaCorrentecliente1 = new ContaCorrente(3004, cliente1);
+const contaCorrentecliente2 = new ContaCorrente(3004, cliente2);
 
-// Dados do cliente número um.
-cliente1.nome = "Yorick";
-cliente1.cpf = 65675778987;
-
-contaCorrentecliente1.agencia = 3004;
-contaCorrentecliente1._saldo = 200;
-contaCorrentecliente1.cliente = cliente1;
-
-// Dados do cliente número dois.
-
-/*cliente2.nome = "Hecarim";
-cliente2.cpf = 77733311144;
-
-contaCorrentecliente2.agencia = 3004;
+//PRIMEIRA FORMA
+/*contaCorrentecliente2.agencia = 3004;
 contaCorrentecliente2.cliente = cliente2;
-contaCorrentecliente2._saldo = 0;*/
+contaCorrentecliente2._saldo = 0;
 
-// Mesmo resultado que anteriormente porém feito de forma mais direta.
-contaCorrentecliente2.cliente = new Cliente();
-contaCorrentecliente2.cliente.nome = "Hecarim";
-contaCorrentecliente2.cliente.cpf = 77733311144;
+//SEGUNDA FORMA
+contaCorrentecliente2._cliente = new Cliente();
+contaCorrentecliente2._cliente.nome = "Hecarim";
+contaCorrentecliente2._cliente.cpf = 77733311144;
 contaCorrentecliente2.agencia = 3001;
-contaCorrentecliente2._saldo = 200;
+contaCorrentecliente2._saldo = 200;*/
 
 
-console.log(contaCorrentecliente2);
+console.log(ContaCorrente.numeroDeContas);
