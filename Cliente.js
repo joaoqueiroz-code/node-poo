07330d50b58523b24ccde03b11bed650514ca4a8
João Queiroz <joaoqueiroz-code@gmail.com>
2021-06-11 09:04:44 -0300

    add special method constructor

--- Cliente.js
+++ Cliente.js
@@ -1,7 +1,17 @@
 // Classe dos dados do cliente.
 export class Cliente{
     nome;
-    cpf;
+    _cpf;
+    
+    get get(){
+        return this._cpf;
+
+    }
+
+    constructor(nome, cpf){
+        this.nome = nome;
+        this._cpf = cpf;
+    }
 
 }
 
